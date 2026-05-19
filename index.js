9d8581df4b803bd1fbdf116496afbd99e4a20bea
Javier Andrade Cobo <javier@backendstudio.com>
2026-05-19 15:33:57 -0500

    New index.js and wrangler.jsonc files

---
+++ index.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env, ctx) {
+    const githubUrl = "https://raw.githubusercontent.com/BackendStudio/gac-ecuador-conf/refs/heads/main/llms.txt";
+    
+    // El robot va al GitHub de BackendStudio y descarga el archivo
+    const response = await fetch(githubUrl);
+    
+    // Le responde al usuario con el contenido en formato de texto plano
+    return new Response(response.body, {
+      status: response.status,
+      headers: {
+        "content-type": "text/plain; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*", // Permite que las IA lo lean sin bloqueos
+      },
+    });
+  },
+};
